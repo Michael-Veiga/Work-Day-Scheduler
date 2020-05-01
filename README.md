7557b44638e6639ab4fa83ef40198aa1ada9b2d6
Mike V <mvee333@gmail.com>
2020-05-01 11:22:37 -0400

    added comments and readme

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # Work-Day-Scheduler
+
+## Purpose
+The purpose of this assignment was to make an application that allows the user to save an event for each hour of the day. The app will also color code each time block to indicate if the current time is greater than, less than or equal to the current time.
+Finally, the app will store any inputs to local storage if the save button is clicked.
+The app runs in the browser and and uses dynamically updated HTML and CSS powered by jQuery. 
+
+## User Story
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+
+## Acceptance Criteria
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+
+## Summary 
+This assignment was relatively straight forward, however the syntax of the if statements and storage input events we're quite challenging at times. My tutor was instrumental in helping me to understand how to use the parent and sibling attributes effectively, as well as the appropriate usage for parseInt. I referenced the layout of another day-planner's script.js which was extremely helpful, however it had many problems of its own. This is honestly the first homework where i've fully comprehended the efficiency of dry code over wet code. 
+
+## Image of application 
+![Image of work-day-scheduler](/assets/images/work-day-schedule-pic.jpg); 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,51 @@
-// display the date and time using moment
+// ** Layout and concepts referenced from the following source https://github.com/cmelby/DayPlanner/blob/master/script.js **
+// Display the date and time using moment.
+// Create a variable to house the current date and year / possibly time.
 var dateTime = moment().format("MMM Do YYYY");
 console.log(dateTime);
-// make sure that it updates depending on the day
+// Update the currentDay using the dateTime variable.
 $("#currentDay").append(dateTime);
 
+// Create an array of times. Must use military time so that if else statements will work.
 var timeEl = [9, 10, 11, 12, 13, 14, 15, 16, 17]
 updateTime();
 // Each block of scheduler is color coded by time of day
 function updateTime() {
+    // set variable for the current hour
     var currentTime = moment().format("H");
+    // create for loop to loop through array 
     for (var i = 0; i < timeEl.length; i++) {
         console.log($("#" + timeEl[i]));
+        // If statement that makes background color row green if the row time is greater than the current time.
+        // parseInt turns current time from a string to an integer as to make it comparable
         if (timeEl[i] > parseInt(currentTime)) {
             $("#" + timeEl[i]).attr("style", "background-color: #98FB98");
         }
+        // Else if statement that makes row background color red if the row time is less than the current time.
         else if (timeEl[i] < parseInt(currentTime)) {
             $("#" + timeEl[i]).attr("style", "background-color: IndianRed");
         }
+        // Else if statement that makes row background color gray if the row time is equal to the current time.
         else if (timeEl[i] == parseInt(currentTime)) {
             $("#" + timeEl[i]).attr("style", "background-color: lightgray");
         }
     }
 }
 // When you save the event, then the text is saved to local storage
+// Event listener for save buttons
 $(".btn").on("click", function () {
+    // make timEL equal to the parent id attribute of the button
+    // use $(this) to reference event source $(".btn") and enables jquery methods to be used 
     var timeEl = $(this).parent().attr("id");
+    // make textEl equal to the sibling input value of the button 
     var textEl = $(this).siblings("input").val().trim();
     // When refreshing the page, time saved events are still there
+    // Store the time and text elements to local storage so that items remain when page refreshes
     localStorage.setItem(timeEl, textEl);
     console.log(timeEl, textEl);
 });
 
+// Take the child input value and set it to the parent element
 $("#9AM").children("input").val(localStorage.getItem("9AM"));
 $("#10AM").children("input").val(localStorage.getItem("10AM"));
 $("#11AM").children("input").val(localStorage.getItem("11AM"));
